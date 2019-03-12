a1fb1e742aa5ee3e6f5839512688b68f6a9c3b0a
Eugene <gish.ee18@gmail.com>
2019-03-12 15:17:36 +0300

    Использование ссылк всместо дубликатов фалов конфига nginx

--- migrations/config/2_create_ssl_section.py
+++ migrations/config/2_create_ssl_section.py
@@ -5,7 +5,7 @@ class Migration2(Migration):
     async def up(self, latest: int) -> None:
         self.config["ssl"] = {
             "cert-chain-path": "CERT-CHAIN-PATH",
-            "cert-privkey-path": "CERT-PRIVKEY-PATH"
+            "cert-privkey-path": "CERT-PRIVKEY-PATH",
         }
 
     async def down(self) -> None:
