e9147058762feccb083489dbb9672b232756907d
Eugene <gish.ee18@gmail.com>
2019-04-01 11:59:03 +0300

    Добавлены дополнительные проверки в генератор снежинок; Генератор снежинок корректно обрабатывает значение sequence

--- iomirea/models/snowflake.py
+++ iomirea/models/snowflake.py
@@ -20,23 +20,51 @@ SEQUENCE_MASK = -1 ^ (-1 << SEQUENCE_BITS)
 
 class SnowflakeGenerator:
     def __init__(self, worker_id: int = 0, datacenter_id: int = 0):
-        # TODO: range checks for worker_id and datacenter_id
+        if not 0 <= worker_id <= MAX_WORKER_ID:
+            raise ValueError(
+                f"Worker id should be between 0 and {MAX_WORKER_ID}"
+            )
+
+        if not 0 <= datacenter_id <= MAX_DATACENTER_ID:
+            raise ValueError(
+                f"Datacenter id should be between 0 and {MAX_DATACENTER_ID}"
+            )
+
         self.worker_id = worker_id
         self.datacenter_id = datacenter_id
-        # TODO: use last_timestamp, start sequence from 0
-        self.sequence = -1
+
+        self.sequence = 0
+
+        self._last_timestamp = -1
 
     def gen_timestamp(self) -> int:
-        return floor(time.time() * 1000) - EPOCH_OFFSET_MS
+        return floor(time.time() * 1000)
 
-    def update_sequence(self) -> None:
-        self.sequence = (self.sequence + 1) & SEQUENCE_MASK
+    def til_next_ms(self) -> int:
+        timestamp = self.gen_timestamp()
+        while timestamp <= self._last_timestamp:
+            timestamp = self.gen_timestamp()
+
+        return timestamp
 
     def gen_id(self) -> int:
-        self.update_sequence()
+        timestamp = self.gen_timestamp()
+        if timestamp < self._last_timestamp:
+            raise RuntimeError(
+                f"System clock went backwards, unable to generate ids for {self._last_timestamp - timestamp}"
+            )
+
+        if self._last_timestamp == timestamp:
+            self.sequence = (self.sequence + 1) & SEQUENCE_MASK
+            if self.sequence == 0:  # overflow
+                timestamp = self.til_next_ms()
+        else:
+            self.sequence = 0
+
+        self._last_timestamp = timestamp
 
         return int(
-            (self.gen_timestamp() << TIMESTAMP_SHIFT)
+            ((timestamp - EPOCH_OFFSET_MS) << TIMESTAMP_SHIFT)
             | (self.datacenter_id << DATACENTER_SHIFT)
             | (self.worker_id << WORKER_SHIFT)
             | self.sequence
