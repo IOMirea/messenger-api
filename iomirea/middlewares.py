09026133d269464d6e7006c38a593f4fda8c2610
Eugene <gish.ee18@gmail.com>
2019-04-03 20:35:27 +0300

    Корректная обработка @me с использованием токена

--- iomirea/middlewares.py
+++ iomirea/middlewares.py
@@ -1,11 +1,31 @@
+"""
+IOMirea-server - A server for IOMirea messenger
+Copyright (C) 2019  Eugene Ershov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 import asyncio
+import binascii
 
 from typing import Callable, Awaitable
 from aiohttp import web
 
 from log import server_log
 from models import converters
-
+from models.access_token import Token
 
 HandlerType = Callable[[web.Request], Awaitable[web.Response]]
 
@@ -48,7 +68,18 @@ async def match_info_validator(
             continue
 
         if value == "@me":
-            value = "0"  # TODO: get id from token
+            try:
+                token_header = req.headers["Authorization"]
+            except KeyError:
+                raise web.HTTPUnauthorized(
+                    reason="No access token passed, unable to decode @me"
+                )
+
+            try:
+                # not doing propper token checks here, not even token structure
+                value = Token.decode_user_id(token_header.partition(".")[0])
+            except (ValueError, binascii.Error):
+                raise web.HTTPUnauthorized(reason="Bad access token passed")
 
         try:
             req["match_info"][key] = await converters.ID().convert(
