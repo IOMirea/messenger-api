173d0e6dbf9718e0d40af3b0bf7d5da6f60493c1
Eugene <gish.ee18@gmail.com>
2019-04-03 20:53:52 +0300

    Возвращение channel_id при обращении к сообщению

--- iomirea/db/postgres.py
+++ iomirea/db/postgres.py
@@ -1,3 +1,22 @@
+"""
+IOMirea-server - A server for IOMirea messenger
+Copyright (C) 2019  Eugene Ershov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 from typing import Dict
 
 import asyncpg
@@ -62,6 +81,7 @@ class Channel(DBObject):
 class Message(DBObject):
     _keys = {
         "author_id": "author_id",
+        "channel_id": "channel_id",
         "content": "content",
         "edited": "edited",
         "pinned": "pinned",
