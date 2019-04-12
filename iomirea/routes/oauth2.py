668a589ec92eb43260e07d9eff8b9b4d554041d1
Donkot <Kotikteror@yandex.ru>
2019-04-12 16:17:09 +0300

    Сделан нормальный вывод ошибок для /authorize

--- iomirea/routes/oauth2.py
+++ iomirea/routes/oauth2.py
@@ -1,29 +1,25 @@
 """
 IOMirea-server - A server for IOMirea messenger
 Copyright (C) 2019  Eugene Ershov
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
 published by the Free Software Foundation, either version 3 of the
 License, or (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
-
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 
-import re
 import hmac
 import base64
 import secrets
 
 from urllib.parse import urlencode
-from typing import Any, Union, Dict, List
+from typing import Any, Union, Dict
 
 import aiohttp_jinja2
 
@@ -33,78 +29,71 @@ from aiohttp import web
 from models import converters, checks
 from models.access_token import Token
 from utils import helpers
-from errors import ConvertError
-from constants import EXISTING_SCOPES
 from db.postgres import APPLICATION
-
-
-class Scope(converters.Converter):
-    async def _convert(self, value: str, app: web.Application) -> List[str]:
-        scopes: List[str] = []
-
-        for i, scope in enumerate(value.split(" ")):
-            if scope not in EXISTING_SCOPES:
-                raise ValueError
-            if scope not in scopes:
-                scopes.append(scope)
-
-        return scopes
-
-
-class RedirectURI(converters.Converter):
-    URI_REGEX = re.compile(r"[a-z]+://\w+[^#]*", re.IGNORECASE | re.UNICODE)
-
-    def __init__(self, verify: bool = True, **kwargs: Any):
-        super().__init__(**kwargs)
-
-        self.verify = verify
-
-    async def _convert(self, value: str, app: web.Application) -> str:
-        if self.verify:
-            if self.URI_REGEX.fullmatch(value) is None:
-                raise ConvertError("Bad format", overwrite_response=True)
-
-        return value
-
-
-authorize_query_params = {
-    "client_id": converters.ID(),
-    "scope": Scope(default=["user"]),
-    "redirect_uri": RedirectURI(),
-    "state": converters.String(default=""),
-    "response_type": converters.String(
-        checks=[checks.OneOf(["code", "token"])]
-    ),
-}
 
 routes = web.RouteTableDef()
 
 
 @routes.get("/authorize")
-@helpers.query_params(authorize_query_params, unique=True)
 @aiohttp_jinja2.template("auth/oauth_confirmation.html")
 async def authorize(
     req: web.Request
 ) -> Union[Dict[str, Any], web.StreamResponse]:
 
-    query = req["query"]
+    query = req.query
 
-    if query["response_type"] == "code":
+    if "response_type" not in query:
+        return web.json_response(
+            {
+                "error": "invalid_request",
+                "error_description": "The request is missing a required parameter: response_type",
+            },
+            status=400,
+        )
+
+    elif query["response_type"] == "code":
         # TODO: check scope items
         # TODO: check redirect_uri
 
+        for p in ("client_id", "scope", "response_type"):
+            if p not in query:
+                return web.json_response(
+                    {
+                        "error": "invalid_request",
+                        "error_description": f"The request is missing a required parameter: {p}",
+                    },
+                    status=400,
+                )
+
+        try:
+            client_id = await converters.ID().convert(
+                query["client_id"], req.app
+            )
+        except ValueError:
+            return web.json_response(...)
+
         record = await req.config_dict["pg_conn"].fetchrow(
             f"SELECT {APPLICATION} FROM applications_with_owner WHERE id = $1",
-            query["client_id"],
+            client_id,
         )
 
         if record is None:
-            raise web.HTTPBadRequest(
-                reason="Application not found in database"
+            return web.json_response(
+                {
+                    "error": "invalid_client",
+                    "error_description": "Unknown client",
+                },
+                status=400,
             )
 
         if record["redirect_uri"] != query["redirect_uri"]:
-            raise web.HTTPBadRequest(reason="Bad redirect_uri passed")
+            return web.json_response(
+                {
+                    "error": "error_uri",
+                    "error_description": "Redirect uri does not match the client",
+                },
+                status=400,
+            )
 
         session = await get_session(req)
         if "user_id" not in session:
@@ -118,25 +107,32 @@ async def authorize(
         return {
             "redirect_uri": record["redirect_uri"],
             "scope": " ".join(query["scope"]),
-            "state": query["state"],
+            "state": query.get("state", ""),
             "app_name": app["name"],
             "app_author_id": app["owner"]["id"],
             "app_author_name": app["owner"]["name"],
         }
 
     elif query["response_type"] == "token":
-        raise web.HTTPNotImplemented(
-            reason="response_type=token is not supported yet"
+        return web.json_response(
+            {
+                "error": "unsupported_grant_type",
+                "error_description": "response_type=token is not supported yet",
+            },
+            status=400,
         )
 
     else:
-        raise RuntimeError(
-            f"Unknown response_type received: {query['response_type']}"
+        return web.json_response(
+            {
+                "error": "unsupported_grant_type",
+                "error_description": "The authorization grant type is not supported by the authorization server.",
+            },
+            status=400,
         )
 
 
 @routes.post("/authorize")
-@helpers.query_params(authorize_query_params, unique=True)
 async def post_authorize(req: web.Request) -> web.Response:
     session = await get_session(req)
     try:
@@ -144,7 +140,7 @@ async def post_authorize(req: web.Request) -> web.Response:
     except KeyError:
         raise web.HTTPUnauthorized(reason="Bad cookie")
 
-    query = req["query"]
+    query = req.query
     post_data = await req.post()
 
     if not post_data.get("confirm_btn"):
@@ -174,10 +170,10 @@ async def post_authorize(req: web.Request) -> web.Response:
         "grant_type": converters.String(
             checks=[checks.OneOf(["authorization_code", "refresh_token"])]
         ),
-        "code": converters.String(),
-        "redirect_uri": RedirectURI(verify=False),
-        "client_id": converters.ID(),
-        "client_secret": converters.String(),
+        "code": converters.String(default=None),
+        "redirect_uri": converters.String(default=None),
+        "client_id": converters.String(default=None),
+        "client_secret": converters.String(default=None),
     },
     unique=True,
     from_body=True,
@@ -187,13 +183,18 @@ async def token(req: web.Request) -> web.Response:
 
     if query["grant_type"] == "authorization_code":
         # TODO: check client_secret
+        # TODO: The identity of the authorization code to the client
 
         record_key = f"auth_code:{query['code']}"
 
         record = await req.config_dict["rd_conn"].execute("GET", record_key)
         if record is None:
-            raise web.HTTPUnauthorized(
-                reason="Wrong or expired authorization code passed"
+            return web.json_response(
+                {
+                    "error": "invalid_grant",
+                    "error_description": "Wrong or expired authorization code passed",
+                },
+                status=400,
             )
 
         await req.config_dict["rd_conn"].execute("DEL", record_key)
@@ -209,7 +210,13 @@ async def token(req: web.Request) -> web.Response:
         ).hexdigest()
 
         if not hmac.compare_digest(calculated_code, query["code"]):
-            raise web.HTTPUnauthorized(reason="Bad authorization code passed")
+            raise web.json_response(
+                {
+                    "error": " unauthorized_client",
+                    "error_description": "Bad authorization code passed",
+                },
+                status=400,
+            )
 
         user_password = await req.config_dict["pg_conn"].fetchval(
             "SELECT password FROM users WHERE id = $1", user_id
