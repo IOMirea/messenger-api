40261e969abe3706e5c888217b168ad18468f0c0
Eugene <gish.ee18@gmail.com>
2019-04-19 10:10:17 +0300

    Revert "Исправлена пробеляция))"
    This reverts commit 1a90c139b4174e2ed2024e41cfdcbd5914711337.

--- schema.sql
+++ schema.sql
@@ -1,20 +1,17 @@
 CREATE TABLE versions (
-  version SMALLINT NOT NULL,
-  name TEXT PRIMARY KEY NOT NULL
+	version SMALLINT NOT NULL,
+	name TEXT PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE messages (
-  id BIGINT PRIMARY KEY NOT NULL,
-  edit_id BIGINT,
-  channel_id BIGINT NOT NULL,
-  author_id BIGINT NOT NULL,
-  content VARCHAR(2048) NOT NULL,
-  encrypted BOOL NOT NULL DEFAULT false,
-  pinned BOOL NOT NULL DEFAULT false,
-  deleted BOOL NOT NULL DEFAULT false
-    
-  FOREIGN KEY channel_id REFERENCES channels(id) ON DELETE RESTRICT,
-  FOREIGN KEY author_id REFERENCES users(id) ON DELETE RESTRICT,
+	id BIGINT PRIMARY KEY NOT NULL,
+	edit_id BIGINT,
+	channel_id BIGINT NOT NULL,
+	author_id BIGINT NOT NULL,
+	content VARCHAR(2048) NOT NULL,
+	encrypted BOOL NOT NULL DEFAULT false,
+	pinned BOOL NOT NULL DEFAULT false,
+	deleted BOOL NOT NULL DEFAULT false
 );
 
 CREATE INDEX messages_channel_index ON messages(channel_id);
@@ -37,71 +34,57 @@ CREATE VIEW messages_with_author AS
   ON msg.author_id = usr.id;
 
 CREATE TABLE users (
-  id BIGINT PRIMARY KEY NOT NULL,
-  name VARCHAR(128) NOT NULL,
-  channel_ids BIGINT[] NOT NULL DEFAULT ARRAY[]::BIGINT[],
-  last_read_message_ids BIGINT[] NOT NULL DEFAULT ARRAY[]::BIGINT[],
-  bot BOOL NOT NULL,
-  email TEXT NOT NULL UNIQUE,
-  password BYTEA NOT NULL,
-  verified BOOL NOT NULL DEFAULT false
-    
-  FOREIGN KEY channel_ids REFERENCES channels(id) ON DELETE RESTRICT,
-  FOREIGN KEY last_read_message_ids REFERENCES messages(id) ON DELETE RESTRICT,
+	id BIGINT PRIMARY KEY NOT NULL,
+	name VARCHAR(128) NOT NULL,
+	channel_ids BIGINT[] NOT NULL DEFAULT ARRAY[]::BIGINT[],
+	last_read_message_ids BIGINT[] NOT NULL DEFAULT ARRAY[]::BIGINT[],
+	bot BOOL NOT NULL,
+	email TEXT NOT NULL UNIQUE,
+	password BYTEA NOT NULL,
+	verified BOOL NOT NULL DEFAULT false
 );
 
 CREATE UNIQUE users_unique_email_index ON users (email);
 
 CREATE TABLE channels (
-  id BIGINT PRIMARY KEY NOT NULL,
-  name VARCHAR(128),
-  user_ids BIGINT[] NOT NULL,
-  pinned_ids BIGINT[] NOT NULL
-    
-  FOREIGN KEY user_ids REFERENCES users(id) ON DELETE RESTRICT,
-  FOREIGN KEY pinned_ids REFERENCES messages(id) ON DELETE RESTRICT,
+	id BIGINT PRIMARY KEY NOT NULL,
+	name VARCHAR(128),
+	user_ids BIGINT[] NOT NULL,
+	pinned_ids BIGINT[] NOT NULL
 );
 
 CREATE TABLE files (
-  id BIGINT PRIMARY KEY NOT NULL,
-  message_id BIGINT NOT NULL,
-  channel_id BIGINT NOT NULL,
-  name VARCHAR(128) NOT NULL,
-  mime SMALLINT NOT NULL,
-  hash UUID NOT NULL
-    
-  FOREIGN KEY channel_id REFERENCES channels(id) ON DELETE RESTRICT,
-  FOREIGN KEY message_id REFERENCES messages(id) ON DELETE RESTRICT,
+	id BIGINT PRIMARY KEY NOT NULL,
+	message_id BIGINT NOT NULL,
+	channel_id BIGINT NOT NULL,
+	name VARCHAR(128) NOT NULL,
+	mime SMALLINT NOT NULL,
+	hash UUID NOT NULL
 );
 
 CREATE TABLE bugreports (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id BIGINT,
-  report_body TEXT NOT NULL,
-  device_info TEXT NOT NULL,
-    
-  FOREIGN KEY user_id REFERENCES users(id) ON DELETE RESTRICT,
+	id SERIAL PRIMARY KEY NOT NULL,
+	user_id BIGINT,
+	report_body TEXT NOT NULL,
+	device_info TEXT NOT NULL
 );
 
 CREATE TABLE tokens (
-  hmac_component TEXT NOT NULL,
-  user_id BIGINT NOT NULL,
-  app_id BIGINT NOT NULL,
-  create_offset INT NOT NULL,
-  scope TEXT[] NOT NULL
-    
-  FOREIGN KEY user_id REFERENCES users(id) ON DELETE RESTRICT,
-  FOREIGN KEY app_id REFERENCES applications(id) ON DELETE CASCADE,
+	hmac_component TEXT NOT NULL,
+	user_id BIGINT NOT NULL,
+	app_id BIGINT NOT NULL,
+	create_offset INT NOT NULL,
+	scope TEXT[] NOT NULL
 );
 
 CREATE INDEX tokens_hmac_component_index ON tokens(hmac_component);
 
 CREATE TABLE applications (
-  id BIGINT PRIMARY KEY NOT NULL,
-  owner_id BIGING NOT NULL,
-  secret TEXT NOT NULL,
-  redirect_uri TEXT NOT NULL,
-  name VARCHAR(256) NOT NULL
+	id BIGINT PRIMARY KEY NOT NULL,
+	owner_id BIGING NOT NULL,
+	secret TEXT NOT NULL,
+	redirect_uri TEXT NOT NULL,
+	name VARCHAR(256) NOT NULL
 );
 
 CREATE VIEW applications_with_owner AS
